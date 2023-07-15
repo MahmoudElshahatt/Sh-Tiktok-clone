33c12836e6a341a8cf92a73dbcf1d6b15b39bf52
Elshahatt <MahmoudElshahatt>
2023-07-15 07:26:59 -0700

    viewpager

--- app/src/main/java/com/shahott/sh_tiktok_clone/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/shahott/sh_tiktok_clone/ui/profile/ProfileFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.shahott.sh_tiktok_clone.R
 import com.shahott.sh_tiktok_clone.databinding.FragmentHomeBinding
 import com.shahott.sh_tiktok_clone.databinding.FragmentProfileBinding
@@ -14,6 +16,7 @@ import com.shahott.sh_tiktok_clone.ui.home.HomeViewModel
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var pagerAdapter: FragmentPageAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,4 +27,44 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClickListeners()
+        setUpViewPager()
+        toggleTabs()
+    }
+
+    private fun onClickListeners() {
+
+    }
+
+    private fun setUpViewPager() {
+        pagerAdapter = FragmentPageAdapter(childFragmentManager, lifecycle)
+        binding.tebLayout.apply {
+            addTab(newTab().setIcon(R.drawable.ic_grid))
+            addTab(newTab().setIcon(R.drawable.ic_hearts))
+        }
+        binding.viewpager.adapter = pagerAdapter
+    }
+
+    private fun toggleTabs() {
+        binding.apply {
+            tebLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    binding.viewpager.currentItem = tab?.position ?: 0
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
+            viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.tebLayout.selectTab(binding.tebLayout.getTabAt(position))
+                }
+            })
+        }
+    }
 }
+
+
